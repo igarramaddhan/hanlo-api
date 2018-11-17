d62fbfaf00de638bb27470a958cc3206e6cfcf09
Igar Ramaddhan <igarramaddhan@gmail.com>
2018-11-17 10:33:59 +0700

    add getUserChat

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -69,5 +69,60 @@ module.exports = {
     } catch (error) {
       res.status(400).send(error);
     }
+  },
+
+  async getUserChat(req, res) {
+    try {
+      const { auth } = req.body;
+      const { userId } = req.params;
+      const messages = await Message.findAll({
+        where: {
+          [Op.or]: [
+            { from: auth.id, to: userId },
+            { to: auth.id, from: userId }
+          ]
+        }
+      });
+
+      let msg = [];
+      for (let i = 0; i < messages.length; i++) {
+        const el = messages[i];
+
+        // GET current chat friend id
+        let current;
+        if (el.from === auth.id) {
+          current = el.to;
+        } else if (el.to === auth.id) {
+          current = el.from;
+        }
+        let index = msg.findIndex(val => val.user[0].id === current);
+        if (index === -1) {
+          let chat = [];
+          for (let j = 0; j < messages.length; j++) {
+            const element = messages[j];
+            if (
+              (element.from === auth.id && element.to === current) ||
+              (element.to === auth.id && element.from === current)
+            ) {
+              chat.push(element);
+            }
+          }
+          const user = await sequelize.query(
+            'select id,username,displayName from Users where id = :id;',
+            {
+              replacements: { id: current },
+              type: sequelize.QueryTypes.SELECT
+            }
+          );
+          if (chat.length !== 0) msg.push({ chat, user });
+        }
+      }
+
+      res.status(200).send({
+        messages: msg[0]
+      });
+    } catch (error) {
+      res.status(400).send(error);
+    }
   }
 };
--- server/routes/message.js
+++ server/routes/message.js
@@ -6,5 +6,6 @@ const { message } = require('../controllers');
 
 app.post('/', auth, message.create);
 app.get('/', auth, message.get);
+app.get('/user/:userId', auth, message.getUserChat);
 
 module.exports = app;
